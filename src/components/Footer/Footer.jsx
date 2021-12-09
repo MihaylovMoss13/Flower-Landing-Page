d90d7ecad7cedde0bcc80888c595fb347cee08db
Le <hieu.le@accenture.com>
2021-12-09 21:36:32 +0100

    feat: make a footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SectionHeader from "../../SectionHeader/SectionHeader";
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <SectionHeader title="Made in" />
+      <h1 className="location" id="location-text">
+        Rotterdam
+      </h1>
+    </section>
+  );
+};
+
+export default Footer;
